dea96965782e112225a88e4961e6246f63223668
Nic Johnson <nicjohnson145@hotmail.com>
2020-10-06 20:33:37 -0500

    Starting basic subcommand bits

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/nicjohnson145/godot
 
 go 1.15
+
+require (
+	github.com/urfave/cli v1.22.4 // indirect
+	github.com/urfave/cli/v2 v2.2.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+    "os"
+)
+
+func main() {
+    cmdMap := NewCmdMap()
+    cmdMap.makeSubCmd("build", "install", "run")
+    cmdMap.Parse(os.Args)
+}
