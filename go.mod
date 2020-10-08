0689be8315eddd4a5883e42701cc53d6c2a7783b
Nic Johnson <nicjohnson145@hotmail.com>
2020-10-08 18:33:35 -0500

    Add gjson dependency. Basic bits for rendering stubbed out

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module github.com/nicjohnson145/godot
 
 go 1.15
 
-require (
-	github.com/urfave/cli v1.22.4 // indirect
-	github.com/urfave/cli/v2 v2.2.0 // indirect
-)
+require github.com/tidwall/gjson v1.6.1
--- render.go
+++ render.go
@@ -1,5 +1,21 @@
 package main
 
-func RenderTemplates(root string) {
-	// outputRoot := filepath.Join(root, "compiled")
+import (
+	"fmt"
+	"github.com/tidwall/gjson"
+)
+
+type templateData struct {
+	Host string
+}
+
+const myJson = `{"name": {"first": "Nic"}}`
+
+func RenderTemplates(config godotConfig) {
+	// outputRoot := filepath.Join(config.RepoPath, "compiled")
+	// srcRoot := filepath.Join(config.RepoPath, "templates")
+}
+
+func readMappings() {
+	fmt.Println(gjson.Get(myJson, "name.first"))
 }
