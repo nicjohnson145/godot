4639aa58ece5f1bf836fab636edf76562d03356d
Nic Johnson <nicjohnson145@hotmail.com>
2020-12-23 11:13:08 -0600

    A single file can be "rendered" and symlinked

--- internal/file/file.go
+++ internal/file/file.go
@@ -3,37 +3,84 @@ package file
 import (
 	"fmt"
 	"io/ioutil"
-	"path"
+	"os"
+	"os/user"
+	"path/filepath"
+	"strings"
 )
 
-
-type File struct {
-    DestinationName string
-    TemplatePath string
+type HomeDirGetter interface {
+	GetHomeDir() string
 }
 
+type OSHomeDir struct{}
 
+func (o *OSHomeDir) GetHomeDir() (string, error) {
+	usr, err := user.Current()
+	if err != nil {
+		err = fmt.Errorf("could not get value of current user %v", err)
+	}
+	dir := usr.HomeDir
+	return dir, err
+}
 
-func (f *File) Render(buildDir string) error {
-    bytes, err := ioutil.ReadFile(f.TemplatePath)
-    if err != nil {
-        err = fmt.Errorf("could not open %q for reading, %v", f.TemplatePath, err)
-        return err
-    }
-    contents := string(bytes)
+type File struct {
+	DestinationPath string
+	TemplatePath    string
+}
 
-    var destPath string
-    if f.DestinationName == "" {
-        destPath = path.Join(buildDir, path.Base(f.TemplatePath))
-    } else {
-        destPath = path.Join(buildDir, f.DestinationName)
-    }
+func NewFile(template string, destination string, home HomeDirGetter) *File {
+	f := &File{
+		TemplatePath: template,
+		DestinationPath: destination,
+	}
+	substituteTilde(f, home)
 
-    err = ioutil.WriteFile(destPath, []byte(contents), 0644)
-    if err != nil {
-        err = fmt.Errorf("could not open %q for writing, %v", destPath, err)
-        return err
-    }
+	return f
+}
 
-    return nil
+func (f *File) render(buildDir string) error {
+	bytes, err := ioutil.ReadFile(f.TemplatePath)
+	if err != nil {
+		err = fmt.Errorf("could not open %q for reading, %v", f.TemplatePath, err)
+		return err
+	}
+	contents := string(bytes)
+
+	destPath := filepath.Join(buildDir, filepath.Base(f.TemplatePath))
+
+	err = ioutil.WriteFile(destPath, []byte(contents), 0644)
+	if err != nil {
+		err = fmt.Errorf("could not open %q for writing, %v", destPath, err)
+		return err
+	}
+
+	return nil
+}
+
+func (f *File) symlink(buildDir string) error {
+	src := filepath.Join(buildDir, filepath.Base(f.TemplatePath))
+	err := os.Symlink(src, f.DestinationPath)
+	if err != nil {
+		err = fmt.Errorf("unable to symlink %q to %q, %v", src, f.DestinationPath, err)
+	}
+	return err
+}
+
+func (f *File) Build(buildDir string) error {
+	err := f.render(buildDir)
+	if err != nil {
+		return err
+	}
+	err = f.symlink(buildDir)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func substituteTilde(f *File, home HomeDirGetter) {
+	if strings.HasPrefix(f.DestinationPath, "~/") {
+		f.DestinationPath = filepath.Join(home.GetHomeDir(), f.DestinationPath[2:])
+	}
 }
--- internal/file/file_test.go
+++ internal/file/file_test.go
@@ -3,98 +3,116 @@ package file
 import (
 	"io/ioutil"
 	"os"
-	"path"
+	"path/filepath"
 	"reflect"
 	"testing"
 )
 
 func createTempDir(t *testing.T, pattern string) (string, func()) {
-    t.Helper()
+	t.Helper()
 
-    dir, err := ioutil.TempDir("", pattern)
-    if err != nil {
-        t.Fatalf("could not create temp directory %v", err)
-    }
+	dir, err := ioutil.TempDir("", pattern)
+	if err != nil {
+		t.Fatalf("could not create temp directory %v", err)
+	}
 
-    remove := func() {
-        os.RemoveAll(dir)
-    }
+	remove := func() {
+		os.RemoveAll(dir)
+	}
 
-    return dir, remove
+	return dir, remove
 }
 
 func assertDirectoryContents(t *testing.T, dir string, want []string) {
-    t.Helper()
+	t.Helper()
 
-    files, err := ioutil.ReadDir(dir)
-    if err != nil {
-        t.Fatalf("could not read directory %v", err)
-    }
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		t.Fatalf("could not read directory %v", err)
+	}
 
-    var found []string
+	var found []string
 
-    for _, file := range files {
-        found = append(found, file.Name())
-    }
+	for _, file := range files {
+		found = append(found, file.Name())
+	}
 
-    if !reflect.DeepEqual(found, want) {
-        t.Errorf("directory listings not equal got %v want %v", found, want)
-    }
+	if !reflect.DeepEqual(found, want) {
+		t.Errorf("directory listings not equal got %v want %v", found, want)
+	}
 }
 
-func writeData(t *testing.T, path string, data []byte) {
-    t.Helper()
+func writeData(t *testing.T, path string, data string) {
+	t.Helper()
 
-    err := ioutil.WriteFile(path, data, 0777)
-    if err != nil {
-        t.Fatalf("could not write file %v", err)
-    }
+	err := ioutil.WriteFile(path, []byte(data), 0777)
+	if err != nil {
+		t.Fatalf("could not write file %v", err)
+	}
 }
+
+func assertSymlinkTo(t *testing.T, link string, source string) {
+	t.Helper()
+
+	info, err := os.Lstat(link)
+	if err != nil {
+		t.Errorf("error getting link info for %q: %v", link, err)
+	}
+
+	if info.Mode() & os.ModeSymlink == 0 {
+		t.Errorf("%q is not a symlink", link)
+	}
+
+	pointsTo, err := os.Readlink(link)
+	if err != nil {
+		t.Errorf("error reading symlink %q, %v", link, err)
+	}
+
+	if pointsTo != source {
+		t.Errorf("symlink not pointing to correct file, got %q want %q", pointsTo, source)
+	}
+}
+
+type TempHomeDir struct {
+	HomeDir string
+}
+
+func (t *TempHomeDir) GetHomeDir() string {
+	return t.HomeDir
+}
+
 
 func TestFile(t *testing.T) {
-    t.Run("render outputs to build directory", func(t *testing.T) {
-        src, removeSrc := createTempDir(t, "src")
-        defer removeSrc()
+	t.Run("files without templating are copied and symlinked", func(t *testing.T) {
+		src, removeSrc := createTempDir(t, "src")
+		defer removeSrc()
 
-        build, removeBuild := createTempDir(t, "build")
-        defer removeBuild()
+		build, removeBuild := createTempDir(t, "build")
+		defer removeBuild()
 
-        srcFile := path.Join(src, "file_1.txt")
-        err := ioutil.WriteFile(srcFile, []byte("some data"), 0777)
-        if err != nil {
-            t.Fatalf("could not write file %v", err)
-        }
+		home, removeHome := createTempDir(t, "home")
+		defer removeHome()
 
-        f := File{TemplatePath: srcFile}
-        err = f.Render(build)
-        if err != nil {
-            t.Errorf("error rendering %v", err)
-        }
+		writeData(t, filepath.Join(src, "some_file"), "the contents")
 
-        want := []string{"file_1.txt"}
-        assertDirectoryContents(t, build, want)
-    })
+		f := NewFile(
+			filepath.Join(src, "some_file"),
+			"~/.some_other_file",
+			&TempHomeDir{HomeDir: home},
+		)
+		err := f.Build(build)
+		if err != nil {
+			t.Errorf("error building, %v", err)
+		}
 
-    t.Run("output filename can be overridden", func(t *testing.T) {
-        src, removeSrc := createTempDir(t, "src")
-        defer removeSrc()
+		want := []string{".some_other_file"}
+		assertDirectoryContents(t, home, want)
+		assertSymlinkTo(
+			t,
+			filepath.Join(home, ".some_other_file"),
+			filepath.Join(build, "some_file"),
+		)
+	})
 
-        build, removeBuild := createTempDir(t, "build")
-        defer removeBuild()
-
-        srcFile := path.Join(src, "file_1.txt")
-        writeData(t, srcFile, []byte("some data"))
-
-        f := File{
-            TemplatePath: srcFile,
-            DestinationName: "other_name.txt",
-        }
-        err := f.Render(build)
-        if err != nil {
-            t.Errorf("error rendering %v", err)
-        }
-
-        want := []string{"other_name.txt"}
-        assertDirectoryContents(t, build, want)
-    })
 }
+
