927ffb327fdc274c0a66cd1552c88ac44fa6695c
Nic Johnson <nicjohnson145@hotmail.com>
2020-12-29 11:39:00 -0600

    Account for renderer removal, utilize template vars

--- internal/builder/builder.go
+++ internal/builder/builder.go
@@ -1,9 +1,13 @@
 package builder
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
+
 	"github.com/nicjohnson145/godot/internal/config"
-	"github.com/nicjohnson145/godot/internal/util"
 	"github.com/nicjohnson145/godot/internal/file"
+	"github.com/nicjohnson145/godot/internal/util"
 )
 
 type Builder struct {
@@ -12,10 +16,35 @@ type Builder struct {
 
 func (b *Builder) Build() error {
 	conf := config.NewConfig(b.Getter)
-	renderer := file.NewRenderer(conf.Files, conf.DotfilesRoot)
-	err := renderer.Render()
+
+	vars := file.TemplateVars{
+		Target: conf.Target,
+	}
+
+	dir, err := b.ensureBuildDir(*conf)
 	if err != nil {
 		return err
 	}
+
+	for _, fl := range conf.Files {
+		err = fl.Render(dir, vars)
+		if err != nil {
+			err = fmt.Errorf("error rendering template %q, %v", fl.DestinationPath, err)
+			return err
+		}
+
+		err = fl.Symlink(dir)
+		if err != nil {
+			err = fmt.Errorf("error symlinking template %q, %v", fl.DestinationPath, err)
+			return err
+		}
+	}
+
 	return nil
+}
+
+func (b *Builder) ensureBuildDir(conf config.Config) (string, error) {
+	buildDir := filepath.Join(conf.DotfilesRoot, "build")
+	err := os.MkdirAll(buildDir, 0744)
+	return buildDir, err
 }
--- internal/builder/builder_test.go
+++ internal/builder/builder_test.go
@@ -53,4 +53,47 @@ func TestBuilder(t *testing.T) {
 			t.Errorf("unexpected contents, got %q want %q", content, expected)
 		}
 	})
+
+	t.Run("templated integration", func(t *testing.T) {
+		home, remove := help.CreateTempDir(t, "home")
+		defer remove()
+
+		dotPath := filepath.Join(home, "dotfiles")
+		err := os.Mkdir(dotPath, 0744)
+		if err != nil {
+			t.Errorf("error making dir, %v", err)
+		}
+
+		err = os.Mkdir(filepath.Join(dotPath, "templates"), 0744)
+		if err != nil {
+			t.Errorf("error making dir, %v", err)
+		}
+		expected := "host zsh contents"
+		help.WriteData(t, filepath.Join(dotPath, "templates", "dot_zshrc"), `{{ .Target }} zsh contents`)
+		help.WriteConfig(t, home, fmt.Sprintf(`{"target": "host", "dotfiles_root": "%v"}`, dotPath))
+		help.WriteRepoConf(t, dotPath, `{
+			"all_files": {
+				"dot_zshrc": "~/.zshrc"
+			},
+			"renders": {
+				"host": ["dot_zshrc"]
+			}
+		}`)
+
+		b := Builder{Getter: &help.TempHomeDir{HomeDir: home}}
+		err = b.Build()
+		if err != nil {
+			t.Errorf("error encountered, %v", err)
+		}
+
+		bytes, err := ioutil.ReadFile(filepath.Join(home, ".zshrc"))
+		if err != nil {
+			t.Errorf("error reading, %v", err)
+		}
+
+		content := string(bytes)
+		if content != expected {
+			t.Errorf("unexpected contents, got %q want %q", content, expected)
+		}
+	})
 }
