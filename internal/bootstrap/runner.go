8376642197603d37ac4028a6d755ba87cc2b8fec
Nic Johnson <nicjohnson145@hotmail.com>
2021-01-21 16:52:14 -0600

    Start on runner

--- internal/bootstrap/runner.go
+++ internal/bootstrap/runner.go
@@ -15,5 +15,15 @@ func (r runner) RunAll() error {
 }
 
 func (r runner) RunSingle(item Item) error {
-	return nil
+	// Check if the item is already installed, and bail early
+	installed, err := item.Check()
+	if err != nil {
+		return err
+	}
+	if installed {
+		return nil
+	}
+
+	// Otherwise, install it
+	return item.Install()
 }
