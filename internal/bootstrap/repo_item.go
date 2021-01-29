55805c8b31bf559c176caceccfd8d7b30b2d38d4
Nic Johnson <nicjohnson145@hotmail.com>
2021-01-29 17:04:39 -0600

    Stub out repo items

--- internal/bootstrap/repo_item.go
+++ internal/bootstrap/repo_item.go
@@ -1,4 +1,22 @@
 package bootstrap
 
 type repoItem struct {
+	Name string
+	Location string
+}
+
+func NewRepoItem(name string, location string) repoItem {
+	return repoItem{
+		Name: name,
+		Location: location,
+	}
+}
+
+func (i repoItem) Check() (bool, error) {
+	// Check if directory exists and has a .git
+	return false, nil
+}
+
+func (i repoItem) Install() error {
+	return nil
 }
