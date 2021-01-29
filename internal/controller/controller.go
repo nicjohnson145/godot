26d3d8c83307ce2f8b9834f657dbf0540638074d
Nic Johnson <nicjohnson145@hotmail.com>
2021-01-29 14:17:37 -0600

    Simplify the push/pull pattern in a lot of methods
    Use some decorators to reduce duplicate code

--- internal/controller/controller.go
+++ internal/controller/controller.go
@@ -85,53 +85,70 @@ func (c *controller) getTarget(t string) string {
 	return t
 }
 
-func (c *controller) Sync(opts SyncOpts) error {
+func (c *controller) git_pushAndPull(function func()error) error {
 	if err := c.repo.Pull(); err != nil {
 		return err
 	}
-	return c.builder.Build(opts.Force)
+
+	if err := function(); err != nil {
+		return err
+	}
+
+	return c.repo.Push()
+}
+
+func (c *controller) git_pullOnly(function func()error) error {
+	if err := c.repo.Pull(); err != nil {
+		return err
+	}
+
+	return function()
+}
+
+func (c *controller) Sync(opts SyncOpts) error {
+	f := func() error {
+		return c.builder.Build(opts.Force)
+	}
+
+	return c.git_pullOnly(f)
 }
 
 func (c *controller) Import(file string, as string, opts ImportOpts) error {
-	if err := c.repo.Pull(); err != nil {
-		return err
-	}
+	f := func() error {
+		// import the file into the repo
+		if err := c.builder.Import(file, as); err != nil {
+			return err
+		}
 
-	// import the file into the repo
-	err := c.builder.Import(file, as)
-	if err != nil {
-		return err
-	}
+		// Add the file to the repos config
+		name, err := c.config.AddFile(as, file)
+		if err != nil {
+			return err
+		}
 
-	// Add the file to the repos config
-	name, err := c.config.AddFile(as, file)
-	if err != nil {
-		return err
-	}
+		// Potentially add the file to the current target
+		if !opts.NoAdd {
+			if err := c.config.AddTargetFile(c.config.Target, name); err != nil {
+				return err
+			}
+		}
 
-	// Potentially add the file to the current target
-	if !opts.NoAdd {
-		err = c.config.AddTargetFile(c.config.Target, name)
+		// If everything has gone right up to this point, write the config to disk
+		return c.config.Write()
 	}
-
-	// If everything has gone right up to this point, write the config to disk
-	if err == nil {
-		err = c.config.Write()
-	}
-
-	if err := c.repo.Push(); err != nil {
-		return err
-	}
-
-	return err
+	return c.git_pushAndPull(f)
 }
 
 func (c *controller) ShowFilesEntry(target string, w io.Writer) error {
-	if c.targetIsSet(target) {
-		return c.TargetShowFiles(target, os.Stdout)
-	} else {
-		return c.ListAllFiles(os.Stdout)
+	f := func() error {
+		if c.targetIsSet(target) {
+			return c.TargetShowFiles(target, os.Stdout)
+		} else {
+			return c.ListAllFiles(os.Stdout)
+		}
 	}
+
+	return c.git_pullOnly(f)
 }
 
 func (c *controller) ListAllFiles(w io.Writer) error {
@@ -144,101 +161,78 @@ func (c *controller) TargetShowFiles(target string, w io.Writer) error {
 }
 
 func (c *controller) TargetAddFile(target string, args []string) error {
-	target = c.getTarget(target)
+	f := func() error {
+		target = c.getTarget(target)
 
-	if err := c.repo.Pull(); err != nil {
-		return err
-	}
-
-	options := c.config.GetAllTemplateNames()
-	tmpl, err := c.fuzzyOrArgs(args, options)
-	if err != nil {
-		if err == fuzzyfinder.ErrAbort {
-			fmt.Println("Aborting...")
-			return nil
+		options := c.config.GetAllTemplateNames()
+		tmpl, err := c.fuzzyOrArgs(args, options)
+		if err != nil {
+			if err == fuzzyfinder.ErrAbort {
+				fmt.Println("Aborting...")
+				return nil
+			}
+			return err
 		}
-		return err
+
+		if err := c.config.AddTargetFile(target, tmpl); err != nil {
+			return err
+		}
+
+		return c.write()
 	}
 
-	err = c.config.AddTargetFile(target, tmpl)
-	if err != nil {
-		return err
-	}
-
-	err = c.write()
-	if err != nil {
-		return err
-	}
-if err := c.repo.Push(); err != nil {
-		return err
-	}
-	return nil
+	return c.git_pushAndPull(f)
 }
 
 func (c *controller) TargetRemoveFile(target string, args []string) error {
-	target = c.getTarget(target)
+	f := func() error {
+		target = c.getTarget(target)
 
-	if err := c.repo.Pull(); err != nil {
-		return err
-	}
-
-	options := c.config.GetAllTemplateNamesForTarget(target)
-	tmpl, err := c.fuzzyOrArgs(args, options)
-	if err != nil {
-		if err == fuzzyfinder.ErrAbort {
-			fmt.Println("Aborting...")
-			return nil
+		options := c.config.GetAllTemplateNames()
+		tmpl, err := c.fuzzyOrArgs(args, options)
+		if err != nil {
+			if err == fuzzyfinder.ErrAbort {
+				fmt.Println("Aborting...")
+				return nil
+			}
+			return err
 		}
-		return err
+
+		if err := c.config.RemoveTargetFile(target, tmpl); err != nil {
+			return err
+		}
+
+		return c.write()
 	}
 
-	err = c.config.RemoveTargetFile(target, tmpl)
-	if err != nil {
-		return err
-	}
-
-	err = c.write()
-	if err != nil {
-		return err
-	}
-
-	if err := c.repo.Push(); err != nil {
-		return err
-	}
-	return nil
+	return c.git_pushAndPull(f)
 }
 
 func (c *controller) EditFile(args []string, opts EditOpts) error {
-	if err := c.repo.Pull(); err != nil {
-		return err
-	}
-
-	path, err := c.getFile(args)
-	if err != nil {
-		if err == fuzzyfinder.ErrAbort {
-			fmt.Println("Aborting...")
-			return nil
-		}
-		return err
-	}
-
-	err = c.editFile(util.ReplacePrefix(path, "~/", c.config.Home), opts)
-	if err != nil {
-		return err
-	}
-
-	if !opts.NoSync {
-		err = c.Sync(SyncOpts{})
+	f := func() error {
+		path, err := c.getFile(args)
 		if err != nil {
+			if err == fuzzyfinder.ErrAbort {
+				fmt.Println("Aborting...")
+				return nil
+			}
 			return err
 		}
+
+		if err := c.editFile(util.ReplacePrefix(path, "~/", c.config.Home), opts); err != nil {
+			return err
+		}
+
+		if !opts.NoSync {
+			if err := c.Sync(SyncOpts{}); err != nil {
+				return err
+			}
+		}
+
+		return nil
 	}
 
-	if err := c.repo.Push(); err != nil {
-		return err
-	}
-
-	return nil
+	return c.git_pushAndPull(f)
 }
 
 
